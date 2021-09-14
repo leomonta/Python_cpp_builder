9d94ee215f2ee889f649bce3d7fddf091255be64
Leonardo Montagner <leomonta2002@gmail.com>
2021-09-14 22:26:01 +0200

    now it actually compiles and it colors the output if error or warnings are presents

--- cpp_builder.py
+++ cpp_builder.py
@@ -12,13 +12,14 @@
 # TODO check for newer versione of header files (check in every file if that header is included, if it is rebuild)
 # TODO error and warning coloring in the console
 # TODO if error occurs stop compilation and return 1
-# TODO retrive include dirs, libs and args from a file
-# TODO retrive target directories for exe, objects, include and source files
+# Done retrive include dirs, libs and args from a file
+# Done retrive target directories for exe, objects, include and source files
 # TODO support for debug and optimization compilation
 
 import subprocess  # execute command on the cmd / bash / whatever
 import os  # get directories file names
 import json  # parse cpp_builder_config.json
+from colorama import Fore, init
 
 
 def exe_command(command):
@@ -39,11 +40,11 @@ args = ["", ""]
 libraries = ["", ""]
 includes = ""
 srcDirs = []
-to_compile = []
 projectDir = ""
 # 0-> object target Directory, 1 -> exe target Directory, 2 -> exe name
 targetDir = ["", "", ""]
 
+source_files_extensions = ["c", "cpp", "cxx", "c++", "cc", "C"]
 
 def parse_config_json():
 	"""
@@ -87,136 +88,38 @@ def parse_config_json():
 	libraries[0] = libraries[0].removesuffix(" ")  # remove trailing space
 
 
-parse_config_json()
+def main():
+	parse_config_json()
 
-for sdir in srcDirs:
-	for file in os.listdir(projectDir + "/" + sdir):
-		if (file.split(".")[-1] in ["c", "cpp"]):
-			to_compile.append(projectDir + "/" + sdir + "/" + file)
+	os.chdir(projectDir)
 
-for file in to_compile:
-	print(file)
-#	command = f"g++ {args} {includes} -c -o {file}.o {file}"
-	# print(command)
-	#res = exe_command(command)
-	#print(res[0], res[1])
+	init()
 
+	to_compile = [] # contains directory and filename
 
-"""
-import subprocess
-import os
-import sys
-import hashlib
+	# individuates the files that need to be compiled
+	for source_directories in srcDirs: # loop trough all the source files directories
+		for file in os.listdir(source_directories): # loop trough every file of each directory
+			# i need to differentiate differen parts
+			# extension: to decide if it has to be compiled or not and to name it 
+			# filename: everything else of the file name ignoring the extension, useful for naming compilitation files
+			# source dir: necessary for differentiate eventual same-named files on different dirs
+			temp = file.split(".")
+			ext = temp.pop(-1)
+			file_name = "".join(temp)
+			if (ext in source_files_extensions): # check if it is a source file
+				to_compile.append([source_directories, file_name, ext])
 
-sha1 = hashlib.sha1()
-old_hashes = {}
-new_hashes = {}
-temp = []
+	# compile each file and show the output
+	for file in to_compile:
+		command = f"g++ {args[0]} {includes} -c -o {targetDir[0]}/{file[0]}{file[1]}.o {file[0]}/{file[1]}.{file[2]}"
+		print(command)
+		res = exe_command(command)
+		if "error" in res[1]:
+			print(Fore.ERROR, res[0], res[1], Fore.ERROR)
+		elif "warning" in res[1]:
+			print(Fore.YELLOW, res[0], res[1], Fore.WHITE)
+		else:
+			print(res[1])
 
-
-def ismodified(filename):
-	global new_hashes
-	global old_hashes
-
-	if filename in old_hashes.keys():
-		if old_hashes[filename] == new_hashes[filename]:
-			return False
-	return True
-
-
-# create file if it does not exist
-if not os.path.exists("files_hash.txt"):
-	f = open("files_hash.txt", "w")
-	f.close()
-
-
-# read hashes from files and add them to old_hashes array
-with open("files_hash.txt", "r") as f:
-	while True:
-		data = f.readline()
-		if not data:
-			break
-		temp = data.split(":")
-
-		# remove trailing newline
-		temp[1] = temp[1].replace("\n", "")
-		old_hashes[temp[0]] = temp[1]
-
-# mi posiziono sulla cartella in cui ci sono i file da compilare e controllare
-files = os.listdir("src")
-
-# obtain new hashes
-for i in files:
-	with open("src\\" + i, "r+b") as f:
-		sha1.update(f.read())
-
-	# insert in the new_hashes dict the key filename eith the value hash
-	new_hashes[i] = sha1.hexdigest()  # create the new hash
-
-	# i need to re-instanciate the object to empty it
-	sha1 = hashlib.sha1()
-
-out = ["/", "/"]
-errors = 0
-
-print("--Compiling--")  # inizio la fase di compilazione
-for i in files:
-	temp = i.split(".")
-
-	if temp[-1] == "cpp":  # distinguo eventuali file cpp o h
-
-		if ismodified(i):  # controllo che non sia gia stato compilato / non modificato
-
-			command = f"g++ -g3 -std=c++2a -Iinclude -Iext -c -Wconversion -Wshadow -Wextra -o .\\objs\\{temp[0]}.o .\\src\\{temp[0]}.cpp"
-
-			print(command)  # stampo il comando per eventuale debug
-
-			stream = subprocess.Popen(
-				command.split(" "), stderr=subprocess.PIPE, universal_newlines=True)
-			out = stream.communicate()  # eseguo il comando e mi faccio comunicare l'esito
-
-			if len(out[1]) == 0:
-				print(f"{i} compiled successfully")
-			else:
-				print(out[1])
-				if (out[1].find("error:") != -1):
-					errors +=1
-
-# ora lavoro con gli oggetti
-
-if errors != 0:
-	print("skipped the linking process for presence of error in the compiling process")
-	exit(1)
-
-# if no compilation happened cause no file changed
-if out[1] == "/":
-	print("Linking skipped, no compilation happened")
-else:
-	files = os.listdir("objs")
-
-	command = f"g++ -g3 -o exe/Game.exe -Lext/glfw/lib -Lext/glew/lib"
-
-	for i in files:
-		if [i.split(".")[-1] == "o"]:
-			command += f" objs/{i}"
-	command += " -lglfw3 -lopengl32 -lgdi32 -lwinmm -lglew32"
-
-	print("\n--Linking--")
-	print(command)
-
-	stream = subprocess.Popen(command.split(
-		" "), stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
-	out = stream.communicate()
-
-	if len(out[1]) == 0:
-		print(f"Program linked successufully")
-	else:
-		print(out[1])
-		exit(1)
-
-with open("files_hash.txt", "w") as f:
-	for i in new_hashes.keys():
-		f.write(i + ":")
-		f.write(new_hashes[i] + "\n")
-
-"""
+main()
