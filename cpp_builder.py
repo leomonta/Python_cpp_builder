c19c602e89a082dff000abee2820d726eb7562cd
Leonardo Montagner <leomonta2002@gmail.com>
2021-11-19 12:33:44 +0100

    added pretty printing

--- cpp_builder.py
+++ cpp_builder.py
@@ -6,11 +6,11 @@
 # Print out error and warning messages
 # add the args for the link and the compile process
 #
-# TODO compile and link files
+# Done compile and link files
 # TODO check for newer version of source files
 # TODO skip compilation or linking if there are no new or modified files
 # TODO check for newer versione of header files (check in every file if that header is included, if it is rebuild)
-# TODO error and warning coloring in the console
+# Done error and warning coloring in the console
 # TODO if error occurs stop compilation and return 1
 # Done retrive include dirs, libs and args from a file
 # Done retrive target directories for exe, objects, include and source files
@@ -21,17 +21,35 @@ import os  # get directories file names
 import json  # parse cpp_builder_config.json
 from colorama import Fore, init
 
+def print_stdout(mex: tuple[str, str]):
 
-def exe_command(command):
+	out = mex[1].split("\n")[0:-1]
+	
+	for i in range(len(out)):
+		if "error" in out[i]:
+			print(Fore.RED, out[i])
+			return False
+		elif "warning" in out[i]:
+			print(Fore.BLUE, out[i])
+		elif "note" in out[i]:
+			print(Fore.CYAN, out[i])
+		else:
+			print(out[i])
+
+	print(Fore.WHITE)
+
+	return True
+
+
+def exe_command(command: str):
 	"""
 	execute the given command and return the output -> [stdout, stderr]
 	"""
 
 	stream = subprocess.Popen(
 		command.split(" "), stderr=subprocess.PIPE, universal_newlines=True)
-	out = stream.communicate()  # execute the command and get the result
 
-	return out
+	return stream.communicate()  # execute the command and get the result
 
 
 # 0 -> compiler args, 1 -> linker args
@@ -74,7 +92,7 @@ def parse_config_json():
 	# create the includes args -> -Iinclude -Isomelibrary/include -I...
 	for Idir in config_file["Directories"]["includeDir"]:
 		includes += "-I" + Idir + " "
-	includes = includes.removesuffix(" ")  # remove trailing space
+	includes = includes.removesuffix(" ") # remove trailing space
 
 	# source dir where the source code file are located
 	srcDirs = config_file["Directories"]["sourceDir"]
@@ -115,24 +133,25 @@ def main():
 
 	# --- Compiling ---
 
-	print(Fore.CYAN, " --- Compiling ---", Fore.WHITE)
+	print(Fore.GREEN, " --- Compiling ---", Fore.WHITE)
+
+	errors = 0
 
 	# compile each file and show the output
 	for file in to_compile:
 		command = f"g++ {args[0]} {includes} -c -o {targetDir[0]}/{file[0]}{file[1]}.o {file[0]}/{file[1]}.{file[2]}"
 		print(command)
-		res = exe_command(command)
-		if "error" in res[1]:
-			print(Fore.RED, res[1], Fore.WHITE)
-		elif "warning" in res[1]:
-			print(Fore.YELLOW, res[1], Fore.WHITE)
-		else:
-			print(res[1])
+		errors += print_stdout(exe_command(command))
+
 
 
 	# --- Linking ---
 
-	print(Fore.CYAN, " --- Linking ---", Fore.WHITE)
+	if errors > 0:
+		print(f"\n{Fore.RED} --- Linking skipped due to errors in compilation process! ---")
+		return 1
+
+	print(Fore.GREEN, " --- Linking ---", Fore.WHITE)
 
 	Link_cmd = f"g++ {args[1]} -o {targetDir[1]}/{targetDir[2]} {libraries[1]}"
 
@@ -143,14 +162,7 @@ def main():
 	Link_cmd += libraries[0]
 
 	print(Link_cmd)
-	res = exe_command(Link_cmd)
-	
-	if "error" in res[1]:
-		print(Fore.RED, res[1], Fore.WHITE)
-	elif "warning" in res[1]:
-		print(Fore.YELLOW, res[1], Fore.WHITE)
-	else:
-		print(res[1])
+	print_stdout(exe_command(Link_cmd))
 
 
 main()
